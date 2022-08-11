5591d5cf0e7687283b1a1a6c9aafc59e0f25a705
samyukta03 <samyuktapurush@gmail.com>
2022-08-11 11:28:22 +0530

    new changes

---
+++ DS/ll-add-two-num.cpp
@@ -0,0 +1,7 @@
+class Solution{
+    public:
+    ListNode* addlists(ListNode* l1, ListNode* l2){
+
+        
+    }
+}
--- DS/lowest-cmn-ancestor-Trees(5).cpp
+++ DS/lowest-cmn-ancestor-Trees(5).cpp
@@ -1,4 +1,6 @@
-/* Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+/* 
+1. Lowest common ancestor - Binary search tree
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
 
 According to the definition of LCA on Wikipedia: The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself)
 */
@@ -17,3 +19,8 @@ public:
      if(p->val > root->val && q->val < root->val || p->val < root->val && q->val > root->val ) return root;
         return root;
 };
+
+/*
+2. Lowest Common ancestor - Binary tree
+
+*/
