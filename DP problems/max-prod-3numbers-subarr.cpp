f284ce65ab30f92f702f394e5109b11b400d7296
Samyukta Sree P <93928428+samyukta03@users.noreply.github.com>
2022-08-16 12:47:12 +0530

    commit

--- DP problems/max-prod-3numbers-subarr.cpp
+++ DP problems/max-prod-3numbers-subarr.cpp
@@ -1,3 +1,10 @@
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+Example 1:
+Input: nums = [1,2,3,4]
+Output: 24
+// =====> Solution : Brute force: sort and find the 3 maximum ele and 2 min ele 
+Time complexity : O(nlogn) 
+Space complexity ; O(1)
 #include<bits/stdc++.h>
 using namespace std;
 int maxprod3numbers(vector<int>&nums){
@@ -8,13 +15,41 @@ int maxprod3numbers(vector<int>&nums){
     int max1=nums[n-1];
     int max2=nums[n-2];
     int max3=nums[n-3];
-    return max(min1*min2*max1,max1,max2,max3);
+    return max(min1*min2*max1,max1*max2*max3);
 }
 int main(){
     vector<int> nums = {1,2,3,4};
     cout << maxprod3numbers(nums)<<endl;
     return 0;
 }
-
-Time complexity : O(n) 
-Space complexity ; O(1)
+//to reduce the tc to O(n), dont use sorting
+Time complexity : O(n) Space complexity : O(1)
+class Solution{
+    public:
+    int maximumProduct(vector<int>& nums){
+        int max1=INT_MIN;
+        int max2=INT_MIN;
+        int max3=INT_MIN;
+        int min1=INT_MAX;
+        int min2=INT_MAX;
+        for(auto n:nums){
+        if(nums[i]>max1){
+                max3=max2;
+                max2=max1;
+                max1=nums[i];
+            } else if(nums[i]>max2){
+                max3=max2;
+                max2=nums[i];
+            }else if(nums[i]>max3){
+                max3=nums[i];
+            }
+             if(nums[i]<min1){
+                min2=min1;
+                min1=nums[i];
+            }else if(nums[i]<min2){
+                min2=nums[i];
+            }
+        }
+           return max(min1*min2*max1,max1*max2*max3);
+}
+};
