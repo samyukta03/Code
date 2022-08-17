464897c450b140eb79e75801857007b60dbc682b
Samyukta Sree P <93928428+samyukta03@users.noreply.github.com>
2022-08-17 23:48:24 +0530

    commit

--- DS/placevalue.cpp
+++ DS/placevalue.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int placeval(int num,int val){
     int place=1,rem=0;
-    while(num>0){
+    while(num > 0){
         rem - num % 10;
         num/=10;
         if(rem == val){
             return place*rem; //if the val is eqal, mult the val wiht the place val got till now and print it 
+            break;
         }
         place*=10; //for every place val, increase it by mult by 10
     }
