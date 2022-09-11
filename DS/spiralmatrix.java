a1c7b8a12f797fadb5b90808e497d1d2d3cf0149
Samyukta Sree P <93928428+samyukta03@users.noreply.github.com>
2022-09-11 16:43:05 +0530

    updtd

--- DS/spiralmatrix.java
+++ DS/spiralmatrix.java
@@ -8,17 +8,22 @@ class Solution {
     int rs = 0, cs = 0, re = n - 1, ce = n - 1;
     ArrayList<Integer> ans = new ArrayList<>();
     while (rs >= 0 && re <= n - 1 && cs >= 0 && ce <= n - 1) {
+        //left to right so row is const and is top most row nd clm is moved cont, after row is moved one down
       for (int i = cs; i <= ce; i++) ans.add(matrix[rs][i]);
       rs++;
+      //top to bottom, so clm is constant and its end clm and now move the end clm to 2nd last 
       for (int i = rs; i <= re; i++) ans.add(matrix[i][ce]);
       ce--;
+
       if (rs < 0 || re > n - 1 || cs < 0 || cs > n - 1) break;
+
+      //again from right to left so row is constant and its the bottom row, after that move thr row one up
       for (int i = ce; i >= cs; i--) ans.add(matrix[re][i]);
       re--;
+      //again from bottom to top so same clm and its starting clm, after that move to one clm right again 
       for (int i = re; i >= rs; i--) ans.add(matrix[i][cs]);
       cs++;
     }
-
     return ans;
   }
 }
