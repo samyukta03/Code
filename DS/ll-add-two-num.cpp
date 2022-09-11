f85c511a108a10e2c729ec3a455e4ce14b471f8b
Samyukta Sree P <93928428+samyukta03@users.noreply.github.com>
2022-09-11 18:10:13 +0530

    updtd

--- DS/ll-add-two-num.cpp
+++ DS/ll-add-two-num.cpp
@@ -29,3 +29,37 @@ public:
         return head->next; //return head->next as it points to l3's first node wiht value stored
     }
 };
+
+
+//------------java
+/**
+Time complexity : O(\max(m, n))O(max(m,n)). Assume that mm and nn represents the length of l1l1
+ and l2l2 respectively, the algorithm above iterates at most \max(m, n)max(m,n) times.
+
+Space complexity : O(\max(m, n))O(max(m,n)). The length of the new list is at most
+ \max(m,n) + 1max(m,n)+1.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode(0);
+        ListNode head = l3;
+        int sum=0,carry=0;
+        while(l1!=null || l2!=null || carry!=0){
+            sum = (carry + (l1!=null ?l1.val:0) + (l2!=null ?l2.val:0));
+            l3.next = new ListNode(sum%10);
+            l3=l3.next;
+            carry = sum/10;
+            if(l1!=null) l1=l1.next;
+            if(l2!=null) l2=l2.next;
+        }
+        return head.next;
+    }
+}
