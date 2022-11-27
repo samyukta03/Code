8f41758d9d4f66ceefc3856b0227d3ffa67f300a
Samyukta Sree P <93928428+samyukta03@users.noreply.github.com>
2022-11-27 22:40:50 +0530

    updated

--- Problems/electricity_shop.java
+++ Problems/electricity_shop.java
@@ -1,25 +1,52 @@
-import java.io.*;
-public class electricity_shop{
-    public int calculateprice(int[] k,int[] d,int b){
-        int price=1;
-        
-        return price;
-    }
-    public static void main(String[] args){
-        Scanner inst = new Scanner(System.in);
-        System.out.println("Enter budget");
-        int b = inst.nextInt();
-        System.out.println("Enter n ");
-        int n = inst.nextInt();
-        int[] key = new int[n];
-        int[] drive = new int[n];
-        for(int i=0;i<n;i++){
-            System.out.println("Enter keyboard "+i);
-            key[i]= inst.nextInt();
-            System.out.println("Enter drive "+ i);
-            drive[i]= inst.nextInt();           
+package week9;
+
+import java.util.*;
+
+public class electricity_shop {
+        public static int calculateprice(int[] k, int[] d, int b){
+            int price=-1;
+            int n = k.length;
+            ArrayList<Integer>cost = new ArrayList<Integer>(n+n);
+            int a = 0;
+                for (int i : k)
+                    for (int j : d)
+                        cost.add(i+j);
+//            Collections.sort(cost);
+//            for(int i=0;i<cost.size();i++){
+//                if(cost.get(i)>b){
+//                    continue;
+//                }
+//                else if(cost.get(i)<=b){
+//                    price = Math.max(price,cost.get(i));
+//                }
+//            }
+            Collections.sort(cost,Collections.reverseOrder());
+            for(int i=0;i<cost.size();){
+                if(cost.get(i)<=b){
+                    price = Math.max(price,cost.get(i));
+                    break;
+                }
+                else i++;
+
+            }
+            return price;
         }
-        int ans = calculateprice(key,drive,b);
-        System.out.println(ans);
-    }
+        public static void main(String[] args) throws Exception{
+            Scanner inst = new Scanner(System.in);
+//            System.out.println("Enter budget");
+//            int b = inst.nextInt();
+//            System.out.println("Enter n ");
+//            int n = inst.nextInt();
+            int b = 60;
+//            int[] key = new int[n];
+//            int[] drive = new int[n];
+            int[]  key = new int[]{40, 50, 60};
+            int[] drive = new int[]{5,8,12};
+//            for(int i=0;i<n;i++)
+//                key[i]= inst.nextInt();
+//            for(int i=0;i<n;i++)
+//            drive[i]= inst.nextInt();
+            int ans = calculateprice(key,drive,b);
+            System.out.println(ans);
+        }
 }
